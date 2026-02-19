df8fff1fa8930911b3a09b0d1f2490bdc40eb8c3
namvux210-IT <maivunam14@gmail.com>
2026-02-19 17:14:07 +0700

    Update nguonc.py

--- api/nguonc.py
+++ api/nguonc.py
@@ -7,16 +7,14 @@ CORS(app)
 
 @app.route('/api/nguonc')
 def handle():
-    kw = request.args.get('keyword', '')
-    slug = request.args.get('path', '')
-    
-    if slug:
-        url = f"https://phim.nguonc.com/api/film/{slug}"
-    else:
-        # NguonC dùng endpoint /films/search cho tìm kiếm
-        url = f"https://phim.nguonc.com/api/films/search?keyword={kw}"
+    slug = request.args.get('path')
+    kw = request.args.get('keyword')
+    # Link mẫu: https://phim.nguonc.com/api/film/[slug]
+    if slug: url = f"https://phim.nguonc.com/api/film/{slug}"
+    elif kw: url = f"https://phim.nguonc.com/api/films/search?keyword={kw}"
+    else: url = "https://phim.nguonc.com/api/films/phim-moi-cap-nhat"
     
     try:
         return jsonify(requests.get(url, timeout=10).json())
     except:
-        return jsonify({"status": False, "data": []})
+        return jsonify({"data": []})
